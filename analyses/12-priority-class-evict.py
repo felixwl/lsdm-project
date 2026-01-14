3e43febba459e6f92537951b604adb774435c796
Felix Lindström <felixwlindstrom@gmail.com>
2026-01-14 16:08:04 +0100

    Did exercise 12

---
+++ analyses/12-priority-class-evict.py
@@ -0,0 +1,57 @@
+from pyspark import SparkContext
+
+
+# start spark with 1 worker thread
+sc = SparkContext("local[1]")
+sc.setLogLevel("ERROR")
+
+
+# read the input file into an RDD[String]
+wholeFile = sc.textFile("{},{},{},{},{}".format(
+    "./dataset/task_events/part-00085-of-00500.csv.gz",
+    "./dataset/task_events/part-00086-of-00500.csv.gz",
+    "./dataset/task_events/part-00087-of-00500.csv.gz",
+    "./dataset/task_events/part-00088-of-00500.csv.gz",
+    "./dataset/task_events/part-00089-of-00500.csv.gz",
+    ))
+
+filter_by = "time"
+
+# filter out the first line from the initial RDD
+entries = wholeFile.filter(lambda x: not (filter_by in x))
+
+# split each line into an array of items
+entries = entries.map(lambda x : x.split(','))
+
+# keep the RDD in memory
+entries.cache()
+
+job_ID_index = 2
+task_id_index = 3
+event_type_index = 5
+scheduling_class_index = 7
+priority_index = 8
+
+
+task_events = entries.map(
+    lambda x: (
+        "{}_{}".format(x[job_ID_index], x[task_id_index]), x[event_type_index]))
+
+task_priority_class = entries.map(
+    lambda x: (
+        "{}_{}".format(x[job_ID_index], x[task_id_index]), 
+        (x[scheduling_class_index], x[priority_index]))
+    )
+
+task_evictions = task_events.mapValues(lambda x: 1 if x == "2" else 0).reduceByKey(max)
+
+priority_class_evictions = task_priority_class.join(task_evictions).values()
+priority_class_eviction_rates = priority_class_evictions.reduceByKey(lambda x,y : (x + y ) / 2)
+priority_class_eviction_rates = priority_class_eviction_rates.sortBy(lambda x: x[1], ascending=False)
+
+
+for eviction_rate in priority_class_eviction_rates.collect():
+    print(f"Scheduling class: {eviction_rate[0][0]}, Priority: {eviction_rate[0][1]}, Eviction rate: {eviction_rate[1]}")
+
+# prevent the program from terminating immediatly
+input("Press Enter to continue...")
